18dda1d8885bb3a09df5772913724e957f5179bf
werignac <werignac@comcast.net>
2021-10-23 15:30:33 -0600

    Pepper fast Patch

--- Green Reaper/Assets/Scripts/Plants/PepperHealth.cs
+++ Green Reaper/Assets/Scripts/Plants/PepperHealth.cs
@@ -5,14 +5,16 @@ using Buffs;
 
 public class PepperHealth : PlantHealth
 {
-    private static FuncBuff<float> pepperSpeedBuff = new FuncBuff<float>((float speed) => speed * 1.5f, 5f, BuffType.BUFF,() => GameObject.FindWithTag("Player").GetComponent<PlayerController>().TurnOffPepperEffect(), "Pepper Speed Buff");
-    private static FuncBuff<float> pepperMaxVelChangeBuff = new FuncBuff<float>((float velChange) => velChange * 1.5f, 5f, BuffType.BUFF, "Pepper Vel Change Buff");
-
     protected override void OnDeath()
     {
+        Debug.Log("Death");
+
         PlayerController player = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
 
         player.TurnOnPepperEffect();
+
+        FuncBuff<float> pepperSpeedBuff = new FuncBuff<float>((float speed) => speed * 1.5f, 5f, BuffType.BUFF, () => GameObject.FindWithTag("Player").GetComponent<PlayerController>().TurnOffPepperEffect(), "Pepper Speed Buff");
+        FuncBuff<float> pepperMaxVelChangeBuff = new FuncBuff<float>((float velChange) => velChange * 1.5f, 5f, BuffType.BUFF, "Pepper Vel Change Buff");
 
         player.BuffMaxSpeed(pepperSpeedBuff);
         player.BuffMaxVelocityChange(pepperMaxVelChangeBuff);
