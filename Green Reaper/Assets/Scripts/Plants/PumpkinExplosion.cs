aae981622521d5f11fc26c5fba7dc2587df3c1e1
werignac <werignac@comcast.net>
2021-10-24 14:00:32 -0600

    Working on Pumpkin and swing event

--- Green Reaper/Assets/Scripts/Plants/PumpkinExplosion.cs
+++ Green Reaper/Assets/Scripts/Plants/PumpkinExplosion.cs
@@ -2,20 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PumpkinExplosion : MonoBehaviour
+public class PumpkinExplosion : DestroyAfterTime
 {
     public int damage;
-
-    private float time = 1.0f;
-
-    private void Update()
-    {
-
-        time -= Time.deltaTime;
-
-        if (time <= 0)
-            Destroy(gameObject);
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
