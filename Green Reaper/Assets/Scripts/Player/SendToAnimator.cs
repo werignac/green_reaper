0a5b8728cb14b00d4b72a62a71f8ef7713a25d5b
werignac <werignac@comcast.net>
2022-01-13 23:26:36 -0700

    Set Float in Animator for movement animation

---
+++ Green Reaper/Assets/Scripts/Player/SendToAnimator.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class SendToAnimator : MonoBehaviour
+{
+    private Animator animator;
+
+    [SerializeField]
+    private string toSet;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void SendFloatToAnimator(float toSend)
+    {
+        animator.SetFloat(toSet, toSend);
+    }
+}
